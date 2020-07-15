9e8f93db9c371e2fcbe1dedda2b3eef2da393d82
kiily <miguelmarin95@hotmail.com>
2020-07-15 23:07:03 +0100

    play around with snowpack, its super quick

--- src/index.js
+++ src/index.js
@@ -1,11 +1,30 @@
-/**
- * This file is just a silly example to show everything working in the browser.
- * When you're ready to start on your site, clear the file. Happy hacking!
- **/
+const grid = document.querySelector('.grid');
+const gridItems = document.querySelectorAll('.grid-item');
+const rowSize = 5;
 
-import confetti from 'canvas-confetti';
+const setColumns = value => grid.getElementsByClassName.setProperty('--template-columns', value);
 
-confetti.create(document.getElementById('canvas'), {
-  resize: true,
-  useWorker: true,
-})({ particleCount: 200, spread: 200 });
+const positionGridItems = () => {
+    gridItems.forEach((x, i) => {
+        if (document.body.clientWidth < 711) {
+            x.style = '';
+            return;
+        } else {
+            const rowSpan = Math.ceil(
+                gridItems.offsetHeight
+            ) / rowSize;
+            x.style.setProperty('--row-span', `span ${rowSpan}`);
+        }
+    });
+}
+
+function debounce(callback, wait) {
+    let timeout;
+    return (...args) => {
+        const context = this;
+        clearTimeout(timeout);
+        timeout = setTimeout(() => callback.apply(context, args), wait);
+    };
+}
+
+window.addEventListener('resize', debounce(positionGridItems, 20));
